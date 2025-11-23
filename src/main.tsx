3c9edd72ba137b6ceae0bd45ab2bb537a8b5822f
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2025-11-24 00:23:03 +0330

    feat: add tanstack react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+import { routeTree } from "./routeTree.gen";
 import { store } from "./redux/store.ts";
 import "./index.css";
-import App from "./App.tsx";
 
 const queryClient = new QueryClient();
+
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Provider>
